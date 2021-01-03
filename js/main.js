0377aace2efd1f38795d397701aa2604f8fea918
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-01-03 18:57:40 +0200

    first-js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*--------The show menu---------*/
+// We creating a variable called showMenu that is needs to have a toggle id and a nav id.
+// both toggle and nav they take the getElementById() method. The reason is this method returns the element
+// that has the current id attribute with the specific value.It is also know us the manipulate method if you 
+// want to get info from an element on our document.
+// in the return it gives us null if there are no elements with the specific id  we called later
+// however, we need to know that id needs to be unique. If there is more than one element with the specific id
+// then our method returns the first element in the code.
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+}
+// next step we are going to 
